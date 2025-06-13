c00b94d9e5dd0a0014a9624badbef79832fde7b7
任五州 <3116419886@qq.com>
2025-06-13 10:36:15 +0800

    feature: 配置mapper扫描路径

--- user/src/main/java/com/rwz/user/UserApplication.java
+++ user/src/main/java/com/rwz/user/UserApplication.java
@@ -1,5 +1,6 @@
 package com.rwz.user;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * 用户管理启动类
  */
 @SpringBootApplication
+@MapperScan("com.rwz.user.dao.mapper")
 public class UserApplication {
 
     public static void main(String[] args) {
